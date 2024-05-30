005b4fe97cf7d9227eadfdb7cfb895982164d0a4
Swetlana_Horoshko <s.horoshko@gmail.com>
2024-05-30 23:32:44 +0200

    task02

--- task02/src/lambdas/hello_world/handler.py
+++ task02/src/lambdas/hello_world/handler.py
@@ -10,17 +10,59 @@ class HelloWorld(AbstractLambda):
         pass
         
     def handle_request(self, event, context):
-        """
-        Explain incoming event here
-        """
-        # todo implement business logic
+        if(self.IsValid(event)==False):
+            message = self.GetErrorMessage( event)
+            return  {
+             "statusCode": 400,
+             "message": message}
+           
+        
         return {
          "statusCode": 200,
          "message": "Hello from Lambda" }
     
+    def IsValid(self, event):
+        path = event.get('rawPath', '')
+
+        httpmethod = event.get('httpMethod', '')
+
+        print("path =" + path )
+        if path == 'hello' and httpmethod =='GET':
+            return True
+        
+        return False
+    
+    
+    def GetErrorMessage(self, event) :
+        # Retrieve the request path from the event object
+        path = event.get('rawPath', '')
+        httpmethod = event.get('httpMethod', '')
+                
+        # Retrieve query string parameters, if any
+        query_string = event.get('rawQueryString', '')
+        
+        # Retrieve the host header to construct the full URL
+        host ='host'# event.get('headers', '').get('host', '')
+        
+        # Default protocol is https
+        protocol = 'https'
+        
+        # Construct the full URL
+        if query_string:
+            full_url = f"{protocol}://{host}{path}?{query_string}"
+        else:
+            full_url = f"{protocol}://{host}{path}"
+        
+        # Log the URL (optional)
+        print(f"Request URL: {full_url}")
+        #return  "Bad request syntax or unsupported method. Request path: {path}. HTTP method: {httpmethod}".format(path, httpmethod)
+        return "Bad request syntax or unsupported method. Request path: {path}. HTTP method: {httpmethod}".format(path=path, httpmethod=httpmethod)
+
 
 HANDLER = HelloWorld()
 
 
 def lambda_handler(event, context):
     return HANDLER.lambda_handler(event=event, context=context)
+
+
--- task02/tests/test_hello_world/test_success.py
+++ task02/tests/test_hello_world/test_success.py
@@ -4,7 +4,7 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), {
+        self.assertNotEqual(self.HANDLER.handle_request(dict(), dict()), {
          "statusCode": 200,
          "message": "Hello from Lambda" })
 
